24219c16a95fb07e301e9d94c08a6d357755f7af
NicolasU94 <nicolas.s.ugarte@gmail.com>
2021-12-10 10:41:50 -0300

    Finished  Logged Account Nav Stack

---
+++ app/navigations/LoggedAccountStack.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import MyData from "../screens/Auth/MyData";
+import Edit from "../screens/Auth/Edit";
+
+const Stack = createStackNavigator();
+
+export default function AccountStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="datos"
+        component={MyData}
+        options={{ title: "Usuario", headerShown: false }}
+      />
+      <Stack.Screen
+        name="edit"
+        component={Edit}
+        options={{ title: " Editar Informacion ", headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}

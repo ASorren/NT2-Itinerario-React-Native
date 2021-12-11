8dbe1a20bcde876eea4bbc13f51e0890b4d12612
NicolasU94 <nicolas.s.ugarte@gmail.com>
2021-12-11 12:06:55 -0300

    Finished Edit Screen

--- app/screens/Auth/Edit.js
+++ app/screens/Auth/Edit.js
@@ -41,6 +41,36 @@ export default function editInfo(props) {
     }
   };
   return (
-      <h1>Empty html</h1>
-  )
+    <ScrollView style={{ backgroundColor: "white" }}>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <Text style={styles.textTitles}> Editar Información </Text>
+      <EditForm
+        navigation={navigation}
+        route={route}
+        countryNames={countryNames}
+        token={token}
+      />
+    </View>
+  </ScrollView>
+);
+  
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: "80%",
+      alignSelf: "center",
+    },
+    logo: {
+      width: "100%",
+      height: 120,
+      resizeMode: "contain",
+      marginBottom: 20,
+    },
+    textTitles: {
+      textAlign: "center",
+      fontSize: 48,
+      color: "#0B3534",
+    },
+  });

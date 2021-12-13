77d08f6e09f79b4ce183d46bde3471c3182861bf
lukeal90 <lainolucas90@gmail.com>
2021-12-13 14:05:47 -0300

    add itinerary stack

---
+++ app/navigations/ItineraryStack.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import General from "../screens/Attractions/General";
+import CreateItineraty from "../screens/Attractions/CreateItineraty";
+import DetalleAtraccion from "../screens/Attractions/DetalleAtraccion";
+import MyTrip from "../screens/Attractions/MyTrip";
+import SearchAtraccion from "../screens/Attractions/SearchAtraccion";
+
+const Stack = createStackNavigator();
+
+export default function ItineraryStack() {
+        return(
+            <Stack.Navigator>  
+                <Stack.Screen 
+                    name="Inicio" 
+                    component={General} 
+                    options={{title: "Inicio", headerShown: false}}
+                    /> 
+                <Stack.Screen 
+                    name="createItineraty" 
+                    component={CreateItineraty} 
+                    options={{title: "Crear Itinerario", headerShown: false}}
+                    />
+                <Stack.Screen 
+                    name="detalleAtraccion" 
+                    component={DetalleAtraccion} 
+                    options={{title: "Detalle Atraccion", headerShown: true}}/>
+                <Stack.Screen 
+                    name="myTrip" 
+                    component={MyTrip} 
+                    options={{title: "Mi viaje", headerShown: false}}/>
+                <Stack.Screen 
+                    name="searchAtraccion" 
+                    component={SearchAtraccion} 
+                    options={{title: "Buscar Atracciones", headerShown: false}}/> 
+            </Stack.Navigator>
+        )
+    }
